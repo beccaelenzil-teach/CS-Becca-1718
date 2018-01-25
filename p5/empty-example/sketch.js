a1216970ee1403e3d921aedc54db2a4d8a8c2b6d
becca <becca.elenzil@bush.edu>
2018-01-24 21:16:49 -0800

    sketch

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,58 +1,87 @@
 circle_list = []
-n = 200
+n = 50
 h = 700
 w = 1000
 m = 0
+dt = 400
 
 function setup() {
   createCanvas(w,h)
   background(0)
+  fill(random(0,256))
   for(i=0; i<n; i++){
-  	circ = new circle(random(0,w+1),random(0,h+1),random(9,12),random([0,1]),random([-4,-2,2,4]),random([-4,-2,2,4]))
+  	circ = new circle(random(0,w+1),random(0,h+1),random(9,12),random(0,256))//random([-4,-2,2,4]),random([-4,-2,2,4]))
   	circle_list.push(circ)
   }
 
 }
 
 function draw() {
+
+  background(0)
+
   if (mouseIsPressed){
   	clear()
   	background(0)
   	circle_list = []
   	for(i=0; i<n; i++){
-  		circ = new circle(random(0,w+1),random(0,h+1),random(9,12),random([0,1]),random([-4,-2,2,4]),random([-4,-2,2,4]))
+  		circ = new circle(random(0,w+1),random(0,h+1),random(9,12),random(0,256))//,random([-4,-2,2,4]),random([-4,-2,2,4]))
   		circle_list.push(circ)
   	}
   }
+
+ 
   for (i=0; i<n; i++){
-  	if (m % 5 == 0){
-  		circle_list[i].display()
-  		circle_list[i].move()
-  		if (circle_list[i].x > w || circle_list[i].x < 0){
+
+      circle_list[i].display()
+
+      dist = sqrt(Math.pow((mouseX-circle_list[i].x),2) + Math.pow((mouseY-circle_list[i].y),2))
+      if (dist < dt){
+        circle_list[i].move()
+
+        if (circle_list[i].x > mouseX){
+          circle_list[i].speed_x = random(dt-10,dt+10)*1/dist
+        }
+        else{
+          circle_list[i].speed_x = -random(dt-10,dt+10)*1/dist
+        }
+        if (circle_list[i].y > mouseY){
+          circle_list[i].speed_y = random(dt-10,dt+10)*1/dist
+        }
+        else{
+          circle_list[i].speed_y = -random(dt-10,dt+10)*1/dist
+        }
+      }
+      
+      
+  		/*if (circle_list[i].x > w || circle_list[i].x < 0){
   			circle_list[i].change_x()
   		}
   		if (circle_list[i].y > h || circle_list[i].y < 0){
   			circle_list[i].change_y()
   		}
-  	}
+      */
   }
 
   d = random(15,25)
-  ellipse(mouseX,mouseY,d,d)
-  if (m % 1 == 0){
-  	fill(random(0,256),random(0,256),random(0,256))
-  }
+  //fill(random(0,256),random(0,256),random(0,256))
+  fill(100,100,100)
+  //ellipse(mouseX,mouseY,d,d)
+  ellipse(mouseX,mouseY,30,30)
+  	
 
   m+=1
 
 }
 
-function circle(x,y,d,a,speed_x,speed_y){
+//function circle(x,y,d,speed_x,speed_y,fill_color){
+function circle(x,y,d,fill_color){
 	this.x = x
 	this.y = y
 	this.d = d
-	this.speed_x = speed_x;
-	this.speed_y = speed_y;
+	this.speed_x = 1//speed_x;
+	this.speed_y = 1//speed_y;
+  this.fill_color = fill_color
 
 	this.change_x = function(){
 		this.speed_x *= -1
@@ -62,15 +91,16 @@ function circle(x,y,d,a,speed_x,speed_y){
 	}
 
 	this.move = function(){
-		  this.x += random(-this.speed_x, this.speed_x);
-    	this.y += random(-this.speed_y, this.speed_y);
-    	//this.x += this.speed_x
-    	//this.y += this.speed_y
+		  //this.x += random(-this.speed_x, this.speed_x);
+    	//this.y += random(-this.speed_y, this.speed_y);
+      this.x += this.speed_x
+    	this.y += this.speed_y
 	}
 
 	this.display = function() {
 		ellipse(this.x, this.y, this.d, this.d)
-    	fill(random(0,256),random(0,256),random(0,256))
-  	}
+    //fill(random(0,256),random(0,256),random(0,256))
+    fill(fill_color)
+  }
 
 }
